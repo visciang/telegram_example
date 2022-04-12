6fbdb48a43aca23c1a76b284db94fcef2eda8c91
Giovanni Visciano <giovanni.visciano@athonet.com>
2022-04-12 20:52:16 +0200

    update

--- lib/telegram_example/application.ex
+++ lib/telegram_example/application.ex
@@ -3,9 +3,10 @@ defmodule TelegramExample.Application do
 
   @impl Application
   def start(_type, _args) do
-    token = Application.fetch_env!(:telegram_example, :token_counter_bot)
-    max_bot_concurrency = Application.fetch_env!(:telegram_example, :max_bot_concurrency)
-    options = [token: token, max_bot_concurrency: max_bot_concurrency]
+    options = [
+      token: Application.fetch_env!(:telegram_example, :token_counter_bot),
+      max_bot_concurrency: Application.fetch_env!(:telegram_example, :max_bot_concurrency)
+    ]
 
     children = [
       {TelegramExample.Bot.Counter, options}
