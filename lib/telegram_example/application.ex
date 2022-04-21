796707f93090da4d09b2a93d437e6b7899253d70
visciang <giovanni_visciano@yahoo.it>
2022-04-21 18:50:24 +0200

    webhook (#1)

--- lib/telegram_example/application.ex
+++ lib/telegram_example/application.ex
@@ -3,13 +3,18 @@ defmodule TelegramExample.Application do
 
   @impl Application
   def start(_type, _args) do
-    options = [
+    webhook_config = [
+      host: Application.fetch_env!(:telegram_example, :host),
+      local_port: Application.fetch_env!(:telegram_example, :local_port)
+    ]
+
+    bot_config = [
       token: Application.fetch_env!(:telegram_example, :token_counter_bot),
       max_bot_concurrency: Application.fetch_env!(:telegram_example, :max_bot_concurrency)
     ]
 
     children = [
-      {TelegramExample.Bot.Counter, options}
+      {Telegram.Webhook, config: webhook_config, bots: [{TelegramExample.Bot.Counter, bot_config}]}
     ]
 
     opts = [strategy: :one_for_one, name: TelegramExample.Supervisor]
