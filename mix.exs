1d7d19b468df5cb26006221f359321af382c5b2a
Giovanni Visciano <giovanni_visciano@yahoo.it>
2024-10-21 21:50:03 +0200

    update

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule TelegramExample.MixProject do
 
   defp deps do
     [
-      {:telegram, github: "visciang/telegram", tag: "1.2.2"},
+      {:telegram, github: "visciang/telegram", tag: "2.0.0"},
       {:bandit, "~> 1.0"},
       {:finch, "~> 0.16"},
       {:credo, "~> 1.0", only: [:dev], runtime: false},
